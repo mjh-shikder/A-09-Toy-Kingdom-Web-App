d4acdccd0d08b0b99ee1247276a522828bdea274
mjh-shikder <mjhsikder@gmail.com>
2025-11-17 22:49:24 +0600

    Navbar added

--- src/components/Layouts/HomeLayout.jsx
+++ src/components/Layouts/HomeLayout.jsx
@@ -1,16 +1,19 @@
-import React from 'react';
-import { Outlet } from 'react-router';
-import Navbar from '../Navbar';
-import Footer from '../Footer';
+import React from "react";
+import { Outlet } from "react-router";
+import Navbar from "../Navbar";
+import Footer from "../Footer";
+import MyContainer from "../MyContainer";
 
 const HomeLayout = () => {
-    return (
-        <div>
-            <Navbar></Navbar>
-            <Outlet></Outlet>
-            <Footer></Footer>
-        </div>
-    );
+  return (
+    <div>
+      <MyContainer>
+        <Navbar></Navbar>
+        <Outlet></Outlet>
+        <Footer></Footer>
+      </MyContainer>
+    </div>
+  );
 };
 
 export default HomeLayout;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React from "react";
+import Logo from "../assets/Logo.png";
+import { Link, NavLink } from "react-router";
 
 const Navbar = () => {
-    return (
-        <div>
-            Navbar
-        </div>
-    );
+  return (
+    <nav className="pt-2.5 flex justify-between items-center ">
+      <div>
+        <img className="w-36" src={Logo} alt="" />
+      </div>
+      <div className="space-x-3">
+        <NavLink to={'/'} className={({isActive})=> isActive? "bg-primary text-white px-2.5 py-0.5 rounded-lg font-semibold b ": "text-secondary"}>Home</NavLink>
+        <NavLink to={'/profile'} className={({isActive})=> isActive? "bg-primary text-white px-2.5 py-0.5 rounded-lg font-semibold b ": "text-secondary"} >My Profile</NavLink>
+      </div>
+      <div>
+        <Link className="btn btn-primary rounded-xl text-white ">Login</Link>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
