4a4d2e42bb0a96415f994f9363c1cef298896d16
mjh-shikder <mjhsikder@gmail.com>
2025-11-18 23:08:19 +0600

    show less button added

--- src/components/Pages/AllToys.jsx
+++ src/components/Pages/AllToys.jsx
@@ -2,23 +2,32 @@ import React from "react";
 import MyContainer from "../MyContainer";
 import Navbar from "../Navbar";
 import Footer from "../Footer";
-import { useLoaderData } from "react-router";
+import { Link, useLoaderData } from "react-router";
 import Card from "../Card";
 
 const AllToys = () => {
   const data = useLoaderData();
 
   return (
-    <div>
+    <div className="bg-base-200">
       <MyContainer>
-        <Navbar></Navbar>
+              <Navbar></Navbar>
+              <section className="bg-white rounded-xl p-10 mt-10">
+                  
+              <h1 className="text-center text-4xl font-semibold text-primary hover:text-secondary  ">All Toys</h1>
               <div className='grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 mt-10 space-y-10 place-items-center  '>
               {
                data.map(card => <Card card={card} key={card.toyId}></Card>)
               }
               </div>
-        <Footer></Footer>
-      </MyContainer>
+              <div className="flex justify-center ">
+                  <Link to={'/'} className='btn btn-secondary px-13 text-center hover:btn-primary '>Show Less</Link>
+                  </div>
+              </section>
+              <Footer></Footer>
+              
+          </MyContainer>
+          
     </div>
   );
 };
