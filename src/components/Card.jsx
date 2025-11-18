185e2a937820404fdc885da04dc58213b3ad9d49
mjh-shikder <mjhsikder@gmail.com>
2025-11-18 16:36:45 +0600

    Popular card refine and responsive

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -14,22 +14,22 @@ const Card = ({ card }) => {
   console.log(card);
 
   return (
-    <div className="card bg-base-100 w-96 shadow-sm text-accent ">
+    <div className="card bg-base-200 w-96 shadow-sm text-accent ">
       <figure>
         <img src={pictureURL} alt="Shoes" />
       </figure>
       <div className="card-body">
-        <div>
-          <h2 className="cardTitle">{toyName}</h2>
-          <div className="font2 flex items-center font-semibold gap-1">
+        <div className="flex items-center justify-between "> 
+          <h2 className="cardTitle text-primary font-semibold text-2xl ">{toyName}</h2>
+          <div className="font2 flex items-center font-semibold gap-1 text-secondary">
             {" "}
             <LiaStarSolid size={17}></LiaStarSolid> {rating}
           </div>
         </div>
         <p className="cardDes">{description}</p>
         <div className="flex  justify-between items-center">
-          <h3 className="font2">Available Quantit ({availableQuantity})</h3>
-          <h3 className="text-primary hover:text-secondary text-lg font2  ">${price}</h3>
+          <h3 className="font2 text-secondary">Available Quantit ({availableQuantity})</h3>
+          <h3 className="text-primary y text-lg font2  ">${price}</h3>
         </div>
         <button className="btn btn-primary hover:btn-secondary ">
           VIew More
--- src/components/home/Popular.jsx
+++ src/components/home/Popular.jsx
@@ -6,11 +6,17 @@ const Popular = ({ popularData }) => {
     
     
     return (
-        <div>
+        <div className='bg-base-100 py-10 rounded-xl'>
             <h1 className='text-4xl text-center font-semibold text-primary hover:text-secondary '>Popular Toys</h1>
-            {
+            <div className='grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 mt-10 space-y-10 place-items-center  '>
+                
+                {
                 popularData.map(card=><Card card={card}></Card>)
-            }
+                }   
+                
+            
+            </div>
+            
         </div>
     );
 };
