6396234b0094212b124a53ed80a4e0bd94dcccc7
mjh-shikder <mjhsikder@gmail.com>
2025-11-26 07:04:21 +0600

    error page edited

--- src/components/Pages/ErrorPage.jsx
+++ src/components/Pages/ErrorPage.jsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import React from "react";
+import errorImg from "../../assets/error.jpg";
+import MyContainer from "../MyContainer";
 
 const ErrorPage = () => {
-    return (
-        <div>
-            Error Page 404
-        </div>
-    );
+  return (
+    <MyContainer>
+      <div className="w-full px-64 ">
+        <img src={errorImg} alt="" />
+      </div>
+    </MyContainer>
+  );
 };
 
 export default ErrorPage;
